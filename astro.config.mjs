11281c25755fad3b0dd660d0efaa9d33d3faf573
Anastasia Tulentseva <anastasiia.tulentseva@gmail.com>
2026-02-16 11:57:44 +0000

    add robots and analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anastasia.tulentsev.com',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/cv/') && !page.includes('/404'),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
